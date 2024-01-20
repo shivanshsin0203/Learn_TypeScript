0ce983ff845bad0fedce55128ad849570074b81f
Shivansh Singh <singhshivansh12may@gmail.com>
2024-01-20 16:14:02 +0530

    Added typescript npm

---
+++ first.js
@@ -0,0 +1,9 @@
+// Ts accepts any type of data and you can assign any type of data to any variable
+var a = 2;
+a = "sff";
+a = false;
+// Arrays in TS hetroginous and single type
+var arr = [1, 2, 3, "sdf", true];
+var arr1 = [1, 2, 3, 4, 5];
+//tuple
+var arr2 = [1, "sdf"];
