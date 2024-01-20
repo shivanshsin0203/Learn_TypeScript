2a8676889c451c5e7e869f61b80a826657448025
Shivansh Singh <singhshivansh12may@gmail.com>
2024-01-20 10:39:18 +0530

    Arrays in TS

--- first.ts
+++ first.ts
@@ -2,3 +2,10 @@
 let a :any=2;
 a="sff";
 a=false
+// Arrays in TS hetroginous and single type
+
+let arr : any[]=[1,2,3,"sdf",true];
+let arr1 : number[]=[1,2,3,4,5];
+//tuple
+let arr2 : [number,string]=[1,"sdf"];
+ 
